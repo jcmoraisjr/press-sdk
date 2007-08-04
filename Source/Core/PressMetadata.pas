88e01a8ac79ac22966e56757353cff69fc516cf1
Joao Morais <jcmoraisjr@gmail.com>
2007-08-04 23:38:51 +0000

    Removed a workaround in the metadata parser already (and properly) implemented in the Compatibility unit.

--- Source/Core/PressMetadata.pas
+++ Source/Core/PressMetadata.pas
@@ -523,10 +523,6 @@ begin
       Token := Reader.ReadToken;
     end else
       VValue := SPressTrueString;
-    // Workaround
-    if (VValue <> '') and (VValue[1] = VValue[Length(VValue)]) and
-     (VValue[1] in ['''', '"']) then
-      VValue := Copy(VValue, 2, Length(VValue) - 2);
     if not SetPropertyValue(FTarget, VPropertyName, VValue) then
       Reader.ErrorExpected(SPressPropertyNameMsg, VPropertyName);
   end;
